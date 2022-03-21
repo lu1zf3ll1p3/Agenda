e6524e17ec6eb62ed94a5c6a743684e5c1c607e3
Luiz Fellipe <luiz.13.silva@gmail.com>
2022-03-20 20:33:47 -0400

    Incluido template para inserção de eventos na agenda, incluido .gitignore para ignorar os arquivos compilados

--- core/models.py
+++ core/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
-    data_evento = models.DateTimeField(verbose_name='Data do evento')
-    data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data da Criação')
+    data_evento = models.DateTimeField(verbose_name='data_evento')
+    data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
@@ -17,4 +18,4 @@ class Evento(models.Model):
         return self.titulo
 
     def get_data_evento(self):
-        return self.data_evento.strftime('%d/%m/%Y %H:%M H')
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
--- core/views.py
+++ core/views.py
@@ -3,13 +3,14 @@ from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
 # Create your views here.
 
 # def index(request):
 #     return redirect('/agenda/')
 
 def login_user(request):
-   return render(request, 'login.html')
+    return render(request, 'login.html')
 
 
 def logout_user(request):
@@ -29,9 +30,29 @@ def submit_login(request):
             messages.error(request, "Usuário ou Senha inválido")
     return redirect('/')
 
+
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
+
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+        return redirect('/')
